ccb6dcf8994e078bc9929c0dd70509031012b02f
Anapey01 <gabriel.k.anapey@gmail.com>
2026-09-26 17:00:51 +0530

    fix(sentry): export onRequestError hook in instrumentation file

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,4 +1,6 @@
 
+import * as Sentry from '@sentry/nextjs';
+
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
         await import('./sentry.server.config');
@@ -8,4 +10,6 @@ export async function register() {
         await import('./sentry.edge.config');
     }
 }
+
+export const onRequestError = Sentry.captureRequestError;
 
